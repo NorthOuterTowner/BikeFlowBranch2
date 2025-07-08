03b4f72ed1a3cc2ea75939fcac0a7fa000f2f770
liruize830 <23301069@bjtu.edu.cn>
2025-07-08 09:48:45 +0800

    update sql and login

--- backend/router/testRouter.js
+++ backend/router/testRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { genid } = require('../db/dbUtils');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ msg: 'Hello from /api/hello' });
+  res.send({
+    status:200,
+    id:genid.NextId()
+  })
 });
 
 module.exports = router;
