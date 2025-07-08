ff58dbe180a1af3570630cc9151162754871c6d4
ains111111ey <ains1ey@outlook.com>
2025-07-08 16:58:20 +0800

    地图

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,8 @@
 
 <style>
 /* 全局样式重置 */
+/* 在你的全局CSS文件中添加这些样式，或者在App.vue的<style>标签中添加 */
+
 * {
   margin: 0;
   padding: 0;
@@ -14,17 +16,21 @@
 html, body {
   height: 100%;
   width: 100%;
-  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden; /* 防止水平滚动条 */
 }
 
 #app {
-  height: 100vh;
-  width: 100vw;
-  position: relative;
-}
-
-.full-screen {
   height: 100%;
   width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+/* 如果你使用了Vue Router，也重置router-view */
+.router-view {
+  width: 100%;
+  height: 100%;
 }
 </style>
