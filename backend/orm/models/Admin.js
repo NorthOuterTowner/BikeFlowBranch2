a74cd3c2605a9d3a513862f20d64a7aadca18099
liruize830 <23301069@bjtu.edu.cn>
2025-07-10 14:36:04 +0800

    代码重构

--- backend/orm/models/Admin.js
+++ backend/orm/models/Admin.js
@@ -3,30 +3,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   const Admin = sequelize.define('Admin', {
     account: {
-      type: DataTypes.STRING, // VARCHAR对应Sequelize中的STRING
-      allowNull: false,      // 假设account不能为空
-      collate: 'latin1_swedish_ci' // 设置校对规则
+      type: DataTypes.STRING,
+      allowNull: false, 
+      collate: 'latin1_swedish_ci'
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,       // 假设password不能为空
+      allowNull: false, 
       collate: 'latin1_swedish_ci'
     },
     token: {
       type: DataTypes.STRING,
       collate: 'latin1_swedish_ci'
-      // allowNull默认为true，即可以为空
     },
     email: {
       type: DataTypes.STRING,
       collate: 'latin1_swedish_ci'
-      // allowNull默认为true，即可以为空
     }
   }, {
-    // 其他模型选项
-    tableName: 'admin', // 明确指定表名
-    charset: 'latin1',  // 设置字符集
-    collate: 'latin1_swedish_ci' // 全局校对规则
+    tableName: 'admin', 
+    charset: 'latin1',
+    collate: 'latin1_swedish_ci' 
   });
 
   return Admin;
